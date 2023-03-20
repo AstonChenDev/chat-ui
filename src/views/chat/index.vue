902e8560f81ab03f563818a6ade923782c4d02e0
ChenPeng <chenpeng@openai.com>
2023-03-20 17:07:32 +0800

    优化

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -482,7 +482,7 @@ onUnmounted(() => {
           <template v-if="!dataSources.length">
             <div class="flex items-center justify-center mt-4 text-center text-neutral-300">
               <SvgIcon icon="ri:bubble-chart-fill" class="mr-2 text-3xl" />
-              <span>Aha~</span>
+              <span>欢迎来到AI宇宙，你可以跟我聊天，比如，写首唐诗……</span>
             </div>
           </template>
           <template v-else>
